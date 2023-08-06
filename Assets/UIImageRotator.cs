bec4867766664b6c7a13828477364c86331fb9e6
ItsAdrift <joshuaking269@gmail.com>
2023-08-07 00:15:10 +1000

    Backup (plz no crash)

--- Assets/UIImageRotator.cs
+++ Assets/UIImageRotator.cs
@@ -6,55 +6,59 @@ public class UIImageRotator : MonoBehaviour
     public Image imageComponent;
     public Sprite[] sprites;
     public float fadeDuration = 1.0f;
+    public float displayDuration = 3.0f; // Time to display each image before cross-fading
 
     private int currentIndex = 0;
-    private Image currentImage;
     private Image nextImage;
     private float fadeTimer = 0f;
+    private float displayTimer = 0f;
+    private bool isFading = false;
 
     private void Start()
     {
-        if (imageComponent == null)
-            imageComponent = GetComponent<Image>();
-
-        // Set the initial sprite
-        imageComponent.sprite = sprites[currentIndex];
-
-        // Create a clone of the UI Image for cross-fading
-        currentImage = Instantiate(imageComponent, imageComponent.transform);
-        currentImage.color = new Color(1f, 1f, 1f, 1f);
-        currentImage.transform.SetAsFirstSibling();
-
-        // Create another clone for the next image
         nextImage = Instantiate(imageComponent, imageComponent.transform);
         nextImage.color = new Color(1f, 1f, 1f, 0f);
+        nextImage.transform.SetParent(imageComponent.transform, false);
 
         UpdateImages();
     }
 
     private void Update()
     {
-        fadeTimer += Time.deltaTime;
-
-        // Cross-fade when the timer reaches the fade duration
-        if (fadeTimer >= fadeDuration)
+        if (!isFading)
         {
-            currentIndex = (currentIndex + 1) % sprites.Length;
-            UpdateImages();
-            fadeTimer = 0f;
+            displayTimer += Time.deltaTime;
+
+            if (displayTimer >= displayDuration)
+            {
+                isFading = true;
+                displayTimer = 0f;
+            }
         }
+        else
+        {
+            fadeTimer += Time.deltaTime;
 
-        // Calculate the alpha values for cross-fading
-        float currentAlpha = Mathf.Lerp(1f, 0f, fadeTimer / fadeDuration);
-        float nextAlpha = Mathf.Lerp(0f, 1f, fadeTimer / fadeDuration);
+            // Cross-fade when the timer reaches the fade duration
+            if (fadeTimer >= fadeDuration)
+            {
+                currentIndex = (currentIndex + 1) % sprites.Length;
+                UpdateImages();
+                fadeTimer = 0f;
+                isFading = false;
+            }
 
-        currentImage.color = new Color(1f, 1f, 1f, currentAlpha);
-        nextImage.color = new Color(1f, 1f, 1f, nextAlpha);
+            // Calculate the alpha values for cross-fading
+            float nextAlpha = Mathf.Lerp(0f, 1f, fadeTimer / fadeDuration);
+
+            nextImage.color = new Color(1f, 1f, 1f, nextAlpha);
+        }
     }
 
     private void UpdateImages()
     {
-        currentImage.sprite = imageComponent.sprite;
-        nextImage.sprite = sprites[currentIndex];
+        imageComponent.sprite = sprites[currentIndex];
+        nextImage.sprite = sprites[(currentIndex + 1) % sprites.Length];
+        nextImage.color = new Color(1f, 1f, 1f, 0f);
     }
 }
